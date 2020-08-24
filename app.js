4c27f44ad7fa92dcb73f49aa896a789d58f549f5
Sedki Sghairi <sedkisghairi017@gmail.com>
2020-08-24 15:24:42 +0400

    trying to play video with js

--- app.js
+++ app.js
@@ -14,4 +14,5 @@ btn.addEventListener('click', function() {
 const preloader = document.querySelector('.preloader');
 window.addEventListener('load', function() {
 	preloader.classList.add('hide-preloader');
+	document.getElementById('video-container').play();
 });
